8cef07ff7c9a10b526e389269c94e8856efadc99
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-09 11:28:54 +0900

    abc

---
+++ delay/set-up.js
@@ -0,0 +1,13 @@
+window.onload = setTimeout(() => {
+    delay();
+}, 2100);
+function delay() {
+    var output = document.getElementById("delay-list").innerHTML;
+    var info0 = "遅延情報無し";
+    var info1 = "";
+    if (info1 = "") {
+        output = info0;
+    } else {
+        output = info1;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ window.onload = setTimeout(() => {
     var out = document.getElementById("day");
     var year = "2023";
     var month = "09";
-    var day = "08";
+    var day = "09";
     out.innerText = year + "/" + month + "/" + day + "更新";
 }, 500);
