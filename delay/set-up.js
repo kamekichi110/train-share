a6ed60b552dcda650bff14fbee1e014e43e2702a
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-09 23:10:56 +0900

    er

--- delay/set-up.js
+++ delay/set-up.js
@@ -1,9 +1,7 @@
 window.onload = setTimeout(() => {
     var output0 = document.getElementById("delay-list");
     var output1 = document.getElementById("stop-list");
-    var info0 = innerDelay;
-    var info1 = innerStop;
-    var NoInfo = "[遅延・運転見合わせ等情報無し]<br>手動入力のため、<br>タイムラグがある可能性があります。";
+    var innerNot = "[遅延・運転見合わせ等情報無し]<br>手動入力のため、<br>タイムラグがある可能性があります。";
     var innerDelay = "JR八高川越線【八王子〜川越】<br>常磐線【品川〜水戸】<br>常磐線(快速)【品川〜取手】<br>両毛線";
     var innerStop = "JR常磐線【水戸〜いわき】<br>久留里線【久留里〜上総亀山】<br>内房線【館山〜安坊鴨川】";
     var update = document.getElementById("UpdateTime");
@@ -13,6 +11,6 @@ window.onload = setTimeout(() => {
     var UpTime = "23:00";
     var UpdateTime = UpTime + "/" + UpMonth + "/" + UpDay + "/" + UpTime;
     update.innerHTML = UpdateTime;
-    output0.innerHTML = info;
-    output1.innerHTML = info0;
-}, 1200);
+    output0.innerHTML = innerDelay;
+    output1.innerHTML = innerStop;
+}, 2000);
