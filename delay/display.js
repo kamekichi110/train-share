dcd74a0cc1c1a852824ef0df04a88a4014e76033
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-11 07:12:42 +0900

    er

--- delay/display.js
+++ delay/display.js
@@ -4,8 +4,20 @@ $(function () {
       cache: false,
       datatype: "html",
       success: function (html) {
-        var html = $(html).find("dd.trouble"); //  別ページにあるhtml内の一部の要素を指定
+        var html = $(html).find(".suspend .normal"); //  別ページにあるhtml内の一部の要素を指定
         $("#yamanote").append(html); // append関数を使って取得したhtmlコードの埋め込み先を指定
       },
     });
   });
+
+  $(function () {
+    $.ajax({
+    url: "https://transit.yahoo.co.jp/diainfo/25/0", // 別ページにあるhtmlページURL
+    cache: false,
+    datatype: "html",
+    success: function (html) {
+    var html = $(html).find(".normal .suspend"); //  別ページにあるhtml内の一部の要素を指定
+    $("#ssl").append(html); // append関数を使って取得したhtmlコードの埋め込み先を指定
+    },
+    });
+    });
