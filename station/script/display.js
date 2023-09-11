b8de6fea3ab4e0e9e48c6a19be6767da8140e0ad
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-11 21:40:43 +0900

    ええええ

---
+++ station/script/display.js
@@ -0,0 +1,15 @@
+function result() {
+    var ResultData = document.getElementById("result");
+    var SelectLine = document.getElementById("line-list").value;
+
+    var error = "error1000<br>路線情報入力中のため<br>表示できません";
+    //JR東日本路線
+    var yamanote = "東京<br>有楽町<br>新橋<br>浜松町<br>田町<br>高輪ゲートウェイ<br>品川<br>大崎<br>五反田<br>目黒<br>恵比寿<br>渋谷<br>原宿<br>代々木<br>新宿<br>新大久保<br>高田馬場<br>目白<br>池袋<br>大塚<br>巣鴨<br>駒込<br>田端<br>西日暮里<br>日暮里<br>鶯谷<br>上野<br>御徒町<br>秋葉原<br>神田";
+    var keihin = "";
+
+    //東武鉄道路線
+    var tojo = "";
+    var noda = "大宮<br>北大宮<br>大宮公園<br>大和田<br>七里<br>岩槻<br>東岩槻<br>豊春<br>八木崎<br>春日部";
+    if (SelectLine = "yamanote") {
+    }
+}
