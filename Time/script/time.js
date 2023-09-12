07db2b32ffee901f847b993be99c7d488d2f665f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-12 18:29:43 +0900

    er

--- Time/script/time.js
+++ Time/script/time.js
@@ -1,11 +1,11 @@
 window.onload = setTimeout(() => {
-    setTimeout(() => {
-    var TimeList = document.getElementById("TimeList");
-    TimeList.style.display = block;
-    }, 160);
     var now = new Date();
     var time = now.getHours();
     var target = document.getElementById(time);
     target.classList.replace("unJust", "Just");
     alert("情報の読み込み完了しました。");
-}, 200);
+    setTimeout(() => {
+        var TimeList = document.getElementById("TimeList");
+        TimeList.style.display = "block";
+    }, 640);
+}, 1000);
