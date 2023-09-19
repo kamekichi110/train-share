d9301eb81e1109eaf9ed5fad5002c5f8dc842ddf
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-19 11:49:17 +0900

    えrえr

--- position/js/location.js
+++ position/js/location.js
@@ -79,6 +79,116 @@ window.onload = setTimeout(() => {
       } else if (35.8885 < ido && ido < 35.8925 && 139.6265 < keido && keido < 139.6305) {
         result.innerHTML = "北与野"
       }
+      //山手線
+else if (35.6795 < ido && 35.6835 < ido && 139.7645 < keido && keido < 139.7685) {
+  result.innerHTML = "東京駅";
+  } 
+  
+  else if (35.6735 < ido && 35.6775 < ido && 139.7615 < keido && keido < 139.7655) {
+  result.innerHTML = "有楽町駅";
+  } 
+  
+  else if (35.6635 < ido && 35.6675 < ido && 139.7575 < keido && keido < 139.7615) {
+  result.innerHTML = "新橋駅";
+  } 
+  
+  else if (35.6535 < ido && 35.6575 < ido && 139.7545 < keido && keido < 139.7585) {
+  result.innerHTML = "浜松町駅";
+  } 
+  
+  else if (35.6435 < ido && 35.6475 < ido && 139.7455 < keido && keido < 139.7477) {
+  result.innerHTML = "田町駅";
+  } 
+  
+  else if (35.6285 < ido && 35.6325 < ido && 139.7385 < keido && keido < 139.7424) {
+  result.innerHTML = "品川駅";
+  }
+  
+  else if (35.6175 < ido && 35.6215 < ido && 139.7265 < keido && keido < 139.7305) {
+  result.innerHTML = "大崎駅";
+  }
+  
+  else if (35.6245 < ido && 35.6285 < ido && 139.7215 < keido && keido < 139.7255) {
+  result.innerHTML = "五反田駅";
+  }
+  
+  else if (35.6315 < ido && 35.6355 < ido && 139.7135 < keido && keido < 139.7175) {
+  result.innerHTML = "目黒駅";
+  }
+  
+  else if (35.6445 < ido && 35.6485 < ido && 139.7085 < keido && keido < 139.7125) {
+  result.innerHTML = "恵比寿駅";
+  }
+  
+  else if (35.6565 < ido && 35.6605 < ido && 139.6995 < keido && keido < 139.7045) {
+  result.innerHTML = "渋谷駅";
+  }
+  
+  else if (35.6695 < ido && 35.6725 < ido && 139.7005 < keido && keido < 139.7045) {
+  result.innerHTML = "原宿駅";
+  }
+  
+  else if (35.6815 < ido && 35.6855 < ido && 139.7005 < keido && keido < 139.7045) {
+  result.innerHTML = "代々木駅";
+  }
+  
+  else if (35.6885 < ido && 35.6925 < ido && 139.6985 < keido && keido < 139.7045) {
+  result.innerHTML = "新宿駅";
+  }
+  
+  else if (35.6995 < ido && 35.7035 < ido && 139.6975 < keido && keido < 139.70275) {
+  result.innerHTML = "新大久保駅";
+  }
+  
+  else if (35.7105 < ido && 35.7145 < ido && 139.7025 < keido && keido < 139.7055) {
+  result.innerHTML = "高田馬場駅";
+  }
+  
+  else if (35.7195 < ido && 35.7235 < ido && 139.7045 < keido && keido < 139.7085) {
+  result.innerHTML = "目白駅";
+  }
+  
+  else if (35.7295 < ido && 35.7335 < ido && 139.7265 < keido && keido < 139.7305) {
+  result.innerHTML = "大塚駅";
+  }
+  
+  else if (35.7315 < ido && 35.7355 < ido && 139.7375 < keido && keido < 139.7415) {
+  result.innerHTML = "巣鴨駅";
+  }
+  
+  else if (35.7345 < ido && 35.7385 < ido && 139.7425 < keido && keido < 139.7465) {
+  result.innerHTML = "田端駅";
+  }
+  
+  else if (35.7305 < ido && 35.7345 < ido && 139.7645 < keido && keido < 139.7685) {
+  result.innerHTML = "西日暮里駅";
+  }
+  
+  else if (35.7345 < ido && 35.7385 < ido && 139.7425 < keido && keido < 139.7465) {
+  result.innerHTML = "日暮里駅";
+  }
+  
+  else if (35.7185 < ido && 35.7225 < ido && 139.7765 < keido && keido < 139.7805) {
+  result.innerHTML = "鶯谷駅";
+  }
+  
+  else if (35.7115 < ido && 35.7155 < ido && 139.7755 < keido && keido < 139.7795) {
+  result.innerHTML = "上野駅";
+  }
+  
+  else if (35.7055 < ido && 35.7095 < ido && 139.7725 < keido && keido < 139.7765) {
+  result.innerHTML = "御徒町駅";
+  }
+  
+  else if (35.6965 < ido && 35.7005 < ido && 139.7725 < keido && keido < 139.7765) {
+  result.innerHTML = "秋葉原駅";
+  }
+  
+  else if (35.6895 < ido && 35.6935 < ido && 139.7685 < keido && keido < 139.7725) {
+  result.innerHTML = "神田駅";
+  } else if (35.6335 < ido && ido < 35.6375 && 139.7385 < keido && keido < 139.7425) {
+    result.innerHTML = "高輪ゲートウェイ駅"
+  }
       else {
       result.innerHTML ="最寄りの駅が見つかりません";
     };
