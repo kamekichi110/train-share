64335c1524604acefd7df900983feb6931c3a71e
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-21 08:29:19 +0900

    day

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ window.onload = setTimeout(() => {
     var out = document.getElementById("day");
     var year = "2023";
     var month = "09";
-    var day = "18";
-    var time = "10:37"
+    var day = "21";
+    var time = "07:40"
     out.innerText = year + "/" + month + "/" + day + "/" + time + "更新";
 }, 2000);
 window.onload = setTimeout(() => {
