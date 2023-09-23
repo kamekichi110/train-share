56b9ba0fc77322102b7f81b8985fb1ce3b8bbbf1
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-24 05:48:41 +0900

    set

---
+++ search/train-search.js
@@ -0,0 +1,26 @@
+function searchJSON() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ''; // Clear previous results
+
+    fetch('train-info.json') // Assuming data.json is in the same directory
+        .then(response => response.json())
+        .then(data => {
+            const filteredData = data.filter(item => {
+                return item.name.toLowerCase().includes(searchInput);
+            });
+
+            if (filteredData.length === 0) {
+                resultsDiv.innerHTML = '該当するデータがありません。';
+            } else {
+                filteredData.forEach(item => {
+                    const resultItem = document.createElement('div');
+                    resultItem.innerHTML = `<p>車両形式: ${item.name}<br>運用会社: ${item.group}<br>編成: ${item.formation}<br>記録など:<br>${item.record}`;
+                    resultsDiv.appendChild(resultItem);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('データの読み込み中にエラーが発生しました: ' + error);
+        });
+}
