c40633a52b1a3704eac912cc45903342f7b79d63
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-09-28 11:40:14 +0900

    sw

--- change/script.js
+++ change/script.js
@@ -1,15 +1,40 @@
 function searchTRAIN() {
     var result = document.getElementById("result");
+    var ver = document.getElementById("version");
+    result.src = "wait.html";
     var location1 = document.getElementById("location1").value;
     var location2 = document.getElementById("location2").value;
     var set1 = "https://api.ekispert.jp/v1/json/search/course/light?from=";
     var set2 = "&to";
-    var set3 = "&searchType=departure&shinkansen=true&limitedExpress=true&redirect=false&contentsMode=pc&key={ご自身のアクセスキーを入力してください}";
+    var set3 = "&searchType=departure&shinkansen=true&limitedExpress=true&redirect=false&contentsMode=pc&key=LE_DDLV9n4GxgPku";
     var fromData = document.getElementById("from").value;
     var forData = document.getElementById("for").value;
     var fromText = fromData + location1;
     var forText = forData + location2;
     var url = set1 + fromText + set2 + forText + set3;
     var encode = encodeURI(url);
-    result.innerHTML = "<p>" + encode + "</p>"
+    setTimeout(() => {
+    //リクエストオブジェクト生成
+    const xhr = new XMLHttpRequest;
+
+    //リクエスト
+    xhr.open("get", encode);
+
+    //リクエスト送信
+    xhr.send();
+
+    //自動的に実行される関数
+    xhr.onreadystatechange = function () {
+        // readyState XMLHttpRequest の状態 4: リクエストが終了して準備完了
+        // status httpステータス
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const jsonData = JSON.parse(xhr.responseText);
+
+            for (let item of jsonData) {
+                result.src = item.ResourceURI;
+                ver.innerHTML = "API-version:" + item.apiVersion;
+            }
+        }
+    }
+}, 1500);
 }
