cb240aab7c26b1220471279e95dbc755c0892928
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-02 03:26:31 +0900

    set

--- change/v2/search.js
+++ change/v2/search.js
@@ -11,27 +11,30 @@ var text = document.getElementById("from").value;
 var url0 = "https://api.ekispert.jp/v1/json/station?name=";
 var url1 = "&offset=1&limit=100&direction=up&gcs=tokyo&key=";
 var key = "LE_DDLV9n4GxgPku";
-var URL = url0 + text + url1 + key;
+var url = url0 + text + url1 + key;
 selectElement.innerHTML = "";
 
-// メインのJSONファイルを読み込む
-fetch(URL)
-  .then(response => response.json())
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('ネットワークエラーまたはHTTPエラーが発生しました');
+    }
+    return response.json(); // JSONデータを解析してJavaScriptオブジェクトに変換
+  })
   .then(data => {
-    // メインのJSONデータを取得した後の処理
+    // JSONデータを取得した後の処理
     console.log(data);
 
     // JSONデータからOption要素を生成し、select要素に追加する
     data.forEach(item => {
       const optionElement = document.createElement('option');
       optionElement.value = item.ResultSet.Point.Station.code; // Optionの値を設定
-      optionElement.text = item.ResultSet.Point.Station.Name;   // Optionの表示テキストを設定
+      optionElement.text = item.ResultSet.Point.Name;   // Optionの表示テキストを設定
       selectElement.appendChild(optionElement); // select要素にOption要素を追加
     });
   })
   .catch(error => {
-    console.error('メインのJSONファイルの読み込みエラー', error);
+    console.error('JSONデータの読み込みエラー', error);
   });
-
 }
 function box1() {}
