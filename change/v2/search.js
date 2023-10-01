be3bb7bf66dfe6b1ae4da65109c1f79ca1ce0044
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-02 02:51:00 +0900

    set

--- change/v2/search.js
+++ change/v2/search.js
@@ -14,8 +14,13 @@ function box0() {
 
     //if関数で名前重複駅があるか検索するプログラム
     if (text = "大宮") {
-        data.innerHTML = '';
-    } else {
+        data.innerHTML = '<option value="大宮(埼玉県)">大宮(埼玉県)</option><option value="大宮(京都府)">大宮(京都府)</option>';
+    } else if (text = "相生") {
+        data.innerHTML = '<option value="相生(岐阜県)">相生(岐阜県)</option><option value="相生(兵庫県)">相生(兵庫県)</option>';
+    } else if (text = "愛野") {
+        data.innerHTML = '<option value="愛野(静岡県)">愛野(静岡県)</option><option value="愛野(長崎県)">愛野(長崎県)</option>';
+    }
+     else {
         data.innerHTML = '<option value="">名前重複駅なし</option>'
     }
 }
