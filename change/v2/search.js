0cbcc1568d69dd4e8c5687317d222762e12eeeb0
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-02 03:31:21 +0900

    set

--- change/v2/search.js
+++ change/v2/search.js
@@ -37,4 +37,35 @@ fetch(url)
     console.error('JSONデータの読み込みエラー', error);
   });
 }
-function box1() {}
+function box1() {
+    var selectElement = document.getElementById('ForArea');
+var text = document.getElementById("for").value;
+var url0 = "https://api.ekispert.jp/v1/json/station?name=";
+var url1 = "&offset=1&limit=100&direction=up&gcs=tokyo&key=";
+var key = "LE_DDLV9n4GxgPku";
+var url = url0 + text + url1 + key;
+selectElement.innerHTML = "";
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('ネットワークエラーまたはHTTPエラーが発生しました');
+    }
+    return response.json(); // JSONデータを解析してJavaScriptオブジェクトに変換
+  })
+  .then(data => {
+    // JSONデータを取得した後の処理
+    console.log(data);
+
+    // JSONデータからOption要素を生成し、select要素に追加する
+    data.forEach(item => {
+      const optionElement = document.createElement('option');
+      optionElement.value = item.ResultSet.Point.Station.code; // Optionの値を設定
+      optionElement.text = item.ResultSet.Point.Name;   // Optionの表示テキストを設定
+      selectElement.appendChild(optionElement); // select要素にOption要素を追加
+    });
+  })
+  .catch(error => {
+    console.error('JSONデータの読み込みエラー', error);
+  });
+}
