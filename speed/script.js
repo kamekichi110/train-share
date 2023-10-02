8f20d8edc9c5ac5af830c0ae8e3de765b1c4c656
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-02 11:44:57 +0900

    set

---
+++ speed/script.js
@@ -0,0 +1,23 @@
+function test() {
+    var num = 0;
+  var watch_id;
+      watch_id = navigator.geolocation.watchPosition(test2, function(e) { alert(e.message); }, {"enableHighAccuracy": true, "timeout": 2000, "maximumAge": 2000});
+  }
+  
+  function clear() {
+    var num = 0;
+  var watch_id;
+      navigator.geolocation.clearWatch(watch_id);
+  }
+  
+  function test2(position) {
+    var num = 0;
+  var watch_id;
+    var speed = position.coords.speed;
+    var speedNum = speed - 30;
+    var num0 = document.getElementById("speed");
+    num0.style.transform = `rotate(${speedNum}deg)`;
+    document.getElementById("num").innerHTML = speed + " km/h";
+   var date = new Date(position.timestamp);
+    document.getElementById("time").innerHTML = date.toLocaleString();
+  }
