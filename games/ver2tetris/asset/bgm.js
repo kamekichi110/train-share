0691ee86868d368dc06835580631c33403a1e375
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-07 18:06:52 +0900

    set

---
+++ games/ver2tetris/asset/bgm.js
@@ -0,0 +1,5 @@
+window.onload = document.getElementById("button").click;
+function bgm() {
+    document.getElementById("bgm").play();
+    document.getElementById("button").style.display = "none";
+}
