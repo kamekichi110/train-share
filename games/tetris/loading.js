712c7efa9878aa9caead9a5e7649d26daa09be63
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-10 05:25:01 +0900

    set

--- games/tetris/loading.js
+++ games/tetris/loading.js
@@ -1,5 +1,4 @@
 window.onload = setTimeout(() => {
     document.getElementById("loading").style.display = "none";
     document.addEventListener("dblclick", function(e){ e.preventDefault();}, { passive: false });
-    document.getElementById("tetris").play();
 }, 4500);
