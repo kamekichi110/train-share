175840d2d6d822ea0cfc5dcf039cb3b4c8e81bfa
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-09 10:13:39 +0900

    set

---
+++ offline/scirpt.js
@@ -0,0 +1,53 @@
+window.onload = setTimeout(() => {
+    loaded();
+}, 1000);
+
+function loaded() {
+    var g2048 = document.getElementById("g-2048");
+    var tower = document.getElementById("tower");
+    var tetris = document.getElementById("tetris");
+    var osero = document.getElementById("osero");
+    var mine = document.getElementById("mine");
+    var suika = document.getElementById("suika");
+    g2048.style.display = "none";
+    tower.style.display = "none";
+    tetris.style.display = "none";
+    osero.style.display = "none";
+    mine.style.display = "none";
+    suika.style.display = "none";
+}
+function game2048() {
+    loaded();
+    var dataIframe = document.getElementById("g-2048");
+    dataIframe.style.display = "block";
+}
+
+function tower() {
+    loaded();
+    var dataIframe = document.getElementById("tower");
+    dataIframe.style.display = "block";
+}
+
+function tetris() {
+    loaded();
+    var dataIframe = document.getElementById("tetris");
+    dataIframe.style.display = "block";
+}
+
+function osero() {
+    loaded();
+    var dataIframe = document.getElementById("osero");
+    dataIframe.style.display = "block";
+}
+
+function mine() {
+    loaded();
+    var dataIframe = document.getElementById("mine");
+    dataIframe.style.display = "block";
+}
+
+function suika() {
+    loaded();
+    var dataIframe = document.getElementById("suika");
+    dataIframe.style.display = "block";
+}
