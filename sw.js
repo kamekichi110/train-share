2087a5f7b9480e16c18b7c7f469abb97396aef8e
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-09 09:16:56 +0900

    set

--- sw.js
+++ sw.js
@@ -36,9 +36,59 @@ var DISP_VERSION = 'ca-d-v1';
 
 // キャッシュの対象にするディレクトリ（css/jsは個別で追加）
 var resources = [
-  '/',
-  '/games',
-  '/speed'
+  '/app-script/app.js',
+  '/app-script/manifest.json',
+  '/app-script/sw.js',
+  '/asset/icon/favicon.ico',
+  '/asset/icon/main.png',
+  '/css/index.css',
+
+  //games file
+
+  //2048 files
+  '/games/2048/index.html',
+  '/games/2048/css/style.css',
+  '/games/2048/js/script.js',
+  '/games/2048/js/libs/jquery/jquery-2.2.4.min.js',
+
+  //gene files
+  '/games/gene/style.css',
+  '/games/gene/suika.html',
+
+  //mine files
+  '/games/mine/index.html',
+  '/games/mine/main.js',
+  '/games/mine/style.css',
+
+  //osero files
+  '/games/osero/index.html',
+  '/games/osero/main.js',
+  '/games/osero/style.css',
+
+  //suika files
+  '/games/suika/assets/dadaxigua.png',
+  '/games/suika/res/import/0a/0ab855d50.json',
+  '/games/suika/res/import/0d/0d669730c.json',
+  '/games/suika/res/import/0e/0e4bc3b03.json',
+  '/games/suika/res/import/04/049f3a810.json',
+  '/games/suika/res/import/6f/6f801092-0c37-4f30-89ef-c8d960825b36.json',
+  '/games/suika/res/import/07/07ce7530a.json',
+  '/games/suika/res/import/07/079499991.json',
+  '/games/suika/res/import/28/2874f8dd-416c-4440-81b7-555975426e93.json',
+  '/games/suika/res/import/41/417a8a59-c0fc-4767-8be2-bf5124bb4b8d.json',
+  '/games/suika/res/import/79/79eafaef-b7ef-45d9-9c3f-591dc836fc7a.json',
+  '/games/suika/res/import/ec/eca5d2f2-8ef6-41c2-bbe6-f9c79d09c432.json',
+  '/games/suika',
+
+  //tetris files
+  '/games/tetris',
+
+  //tower game files
+  '/games/tower',
+
+  //other
+  '/speed',
+  '/'
 ];
 
 // キャッシュ追加
