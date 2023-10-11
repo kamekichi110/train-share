08e432573d0d0e3380cc5a5a87be945816d11b2f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-11 16:03:23 +0900

    set

---
+++ games/login.js
@@ -0,0 +1,22 @@
+window.onload = setTimeout(() => {
+    var key = "tobu";
+    var num = 8500;
+    var set = 1;
+    var pw = window.prompt("認証IDを入力してください。", a1b2c3d4);
+    if (pw = "f5si") {
+        window.alert("認証完了しました。");
+        var pass1 = 8000 - 54 + 163;
+        var pass2 = pass1 - set;
+        var pass3 = num * 0;
+        var result = pass3 + pass2;
+        var id = key + result + "f";
+        RedirectPage(id);
+    } else if (pw = "t8110f") {
+        window.alert("別ページの認証idです。\n他のものを入力してください。");
+        var id = "0000";
+        RedirectPage(id);
+    } else {
+        var id = "0000";
+        RedirectPage(id);
+    }
+}, 200);
---
+++ games/script.js
@@ -0,0 +1,15 @@
+window.onload = setting();
+function setting() {
+    console.log("page data loaded!");
+}
+function RedirectPage(URL) {
+    var getURL = "f5si"
+    if (URL = "tobu8110f") {
+        var page = document.getElementById("page").value;
+        window.location.href = "https://train-share.f5.si/games/" + URL + page + ".html";
+    } else if(URL = "0000") {
+        window.alert("認証エラーです。\nホームに戻ります。");
+        console.log(getURL);
+        window.location.href = "https://train-share.f5.si/index.html"
+    }
+}
