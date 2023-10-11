3b8a7f99231b4a2d5e094d8af640c0f7239f9579
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-11 16:35:15 +0900

    set

--- games/script.js
+++ games/script.js
@@ -6,7 +6,7 @@ function RedirectPage(URL) {
     var getURL = "f5si"
     if (URL = "tobu8110f") {
         var page = document.getElementById("page").value;
-        window.location.href = "https://train-share.f5.si/games/" + URL + "/" + URL + page + ".html";
+        window.location.href = "https://train-share.f5.si/games/" + page + "/" + URL + page + ".html";
     } else {
         window.alert("ホームに戻ります。");
         console.log(getURL);
