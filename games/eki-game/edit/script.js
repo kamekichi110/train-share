b10b7f137611d87e218d1a161a4a3a8f9ea9a27b
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-12 10:40:03 +0900

    set

--- games/eki-game/edit/script.js
+++ games/eki-game/edit/script.js
@@ -6,8 +6,8 @@ var timerInterval;
 
 function incrementInput() {
     inputValue += 1; // 1を加えます
-    document.getElementById("time").value = inputValue / 10; // HTMLのinput要素に値をセットします
-    setTimeout(incrementInput, 100); // 1ミリ秒後に再度incrementInput関数を呼び出します
+    document.getElementById("time").value = inputValue; // HTMLのinput要素に値をセットします
+    setTimeout(incrementInput, 1); // 1ミリ秒後に再度incrementInput関数を呼び出します
 }
 
 const mp3FileInput = document.getElementById('mp3File');
