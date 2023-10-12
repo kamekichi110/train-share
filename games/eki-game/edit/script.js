6ed26bc9236472dbd5b19e8c250377a75cf2aa48
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-12 11:03:38 +0900

    set

--- games/eki-game/edit/script.js
+++ games/eki-game/edit/script.js
@@ -13,13 +13,13 @@ function incrementInput() {
 const mp3FileInput = document.getElementById('mp3File');
         const audioPlayer = document.getElementById('audioPlayer');
 
-        mp3FileInput.addEventListener('change', function() {
+        function changeMP3() {
             const file = mp3FileInput.files[0];
             if (file) {
                 const objectURL = URL.createObjectURL(file);
                 audioPlayer.src = objectURL;
             }
-        });
+        };
 
         function add(track) {
             const resultDiv = document.getElementById('result');
