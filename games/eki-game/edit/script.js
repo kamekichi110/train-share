68232be5f0bf915c17a234d712ccd22c50cbfa2f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-18 16:23:11 +0900

    set

--- games/eki-game/edit/script.js
+++ games/eki-game/edit/script.js
@@ -27,7 +27,7 @@ function incrementInput() {
             var time = document.getElementById("time").value;
             var setTime = Number(time) * 1000;
             var value0 = parseInt(setTime, 10);
-            resultDiv.innerHTML += '<p>{ "track": ' + `${track}` + ',' + ' "targetTime": ' + `${value0}` + '},</p><br>';
+            resultDiv.innerHTML += '{ "track": ' + `${track}` + ',' + ' "targetTime": ' + `${value0}` + '},<br>';
             console.log('{ "track": ' + `${track}` + ',' + ' "targetTime": ' + `${value0}` + '}');
             download();
         }
