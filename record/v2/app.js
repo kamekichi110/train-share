a51d6cf3d5589b5a76fa6b7bdde08ce68fb18c40
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-10-28 17:13:50 +0900

    set

--- record/v2/app.js
+++ record/v2/app.js
@@ -19,3 +19,17 @@ document.getElementById('uploadForm').addEventListener('submit', function(e) {
         uploadStatus.textContent = 'ネットワークエラーが発生しました。';
     });
 });
+// 非同期関数を使用してディレクトリ容量を取得
+async function getDirectorySize() {
+    try {
+        const response = await fetch('http://trainshare1.starfree.jp/getDirectorySizeScript.php');
+        const data = await response.json();
+        document.getElementById('directorySize').innerHTML = `ディレクトリ容量: ${data.size} bytes`;
+    } catch (error) {
+        console.error('ディレクトリ容量の取得中にエラーが発生しました:', error);
+    }
+}
+window.addEventListener('load', function() {
+    // ここにgetDirectorySize()関数を呼び出すコードを記述
+    getDirectorySize();
+});
